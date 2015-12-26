976bdb9b3336df179d83b39b5fdb43bb5d1792dc
chenshengfa1990 <854852417@qq.com>
2015-12-27 01:34:58 +0800

    upload the first version code 2015/12/27

---
+++ app/src/main/java/com/bookstore/main/FloatButton.java
@@ -0,0 +1,66 @@
+package com.bookstore.main;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * Created by Administrator on 2015/12/6.
+ */
+public class FloatButton extends View {
+    private int mColor = -1;
+    private final Paint pen = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private FrameLayout.LayoutParams layoutParams;
+    public FloatButton(Context context) {
+        this(context, null);
+    }
+
+    public FloatButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FloatButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.FloatingActionButton);
+        mColor = a.getColor(R.styleable.FloatingActionButton_bgColor, Color.WHITE);
+        pen.setStyle(Paint.Style.FILL);
+        pen.setColor(mColor);
+
+        float radius, dx, dy;
+        int shadowColor;
+        radius = a.getFloat(R.styleable.FloatingActionButton_shadowRadius, 10.0f);
+        dx = a.getFloat(R.styleable.FloatingActionButton_shadowDx, 0);
+        dy = a.getFloat(R.styleable.FloatingActionButton_shadowDy, 0);
+        shadowColor = a.getColor(R.styleable.FloatingActionButton_shadowColor, Color.argb(100, 0, 0, 0));
+        pen.setShadowLayer(radius, dx, dy, shadowColor);
+        a.recycle();
+
+        int size = context.getResources().getDimensionPixelSize(R.dimen.sub_float_button_size);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(size, size, Gravity.TOP | Gravity.LEFT);
+        setLayoutParams(params);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        canvas.drawCircle(getWidth()/2, getHeight()/2, (float)(getWidth()/2.6), pen);
+
+    }
+
+    public void setLayoutParams(FrameLayout.LayoutParams params)
+    {
+        this.layoutParams = params;
+    }
+
+    public FloatButton addSubFloatButton(View subFloatButton)
+    {
+        return this;
+    }
+}
