5761d95276ec95954d9e5edad7140ddde2177162
chenshengfa <854852417@qq.com>
2016-03-08 00:04:11 +0800

    add content provider

---
+++ app/src/main/java/com/bookstore/provider/BookProvider.java
@@ -0,0 +1,48 @@
+package com.bookstore.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2016/3/7.
+ */
+public class BookProvider extends ContentProvider {
+    public static final String AUTHORITY = "com.bookstore.provider.BookProvider";
+    private static final String DATABASE_NAME = "BookProvider.db";
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
