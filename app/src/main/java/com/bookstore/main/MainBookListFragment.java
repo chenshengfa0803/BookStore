1b6ca0f8b2673fba4d82244a45d60b2bfd617c6e
chenshengfa <854852417@qq.com>
2016-04-21 02:58:11 +0800

    [onGoing] add searchView into toolbar

--- app/src/main/java/com/bookstore/main/MainBookListFragment.java
+++ app/src/main/java/com/bookstore/main/MainBookListFragment.java
@@ -6,13 +6,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.transition.Fade;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -138,6 +141,12 @@ public class MainBookListFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setQueryHint("书名/作者");
+
+        //searchView.setIconifiedByDefault(false);
+        //searchView.setSubmitButtonEnabled(true);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
