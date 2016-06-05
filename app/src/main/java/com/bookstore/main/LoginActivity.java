950de3d78b9c34d322942034f9deb2ea2b20fd1b
chenshengfa <854852417@qq.com>
2016-06-05 20:14:51 +0800

    Add Sina login

--- app/src/main/java/com/bookstore/main/LoginActivity.java
+++ app/src/main/java/com/bookstore/main/LoginActivity.java
@@ -2,14 +2,48 @@ package com.bookstore.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.bookstore.main.sina.Constants;
+import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.WeiboAuthListener;
+import com.sina.weibo.sdk.auth.sso.SsoHandler;
+import com.sina.weibo.sdk.exception.WeiboException;
 
 /**
  * Created by Administrator on 2016/6/4.
  */
 public class LoginActivity extends Activity {
+    private AuthInfo mAuthInfo;
+    private SsoHandler mSsoHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        mAuthInfo = new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE);
+        mSsoHandler = new SsoHandler(this, mAuthInfo);
+
+        findViewById(R.id.login_with_sina).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSsoHandler.authorize(new WeiboAuthListener() {
+                    @Override
+                    public void onComplete(Bundle bundle) {
+
+                    }
+
+                    @Override
+                    public void onWeiboException(WeiboException e) {
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+                });
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/bookstore/main/sina/Constants.java
@@ -0,0 +1,13 @@
+package com.bookstore.main.sina;
+
+/**
+ * Created by Administrator on 2016/6/5.
+ */
+public class Constants {
+    public static final String APP_KEY = "3576656105";
+    public static final String REDIRECT_URL = "https://api.weibo.com/oauth2/default.html";
+    public static final String SCOPE =
+            "email,direct_messages_read,direct_messages_write,"
+                    + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
+                    + "follow_app_official_microblog," + "invitation_write";
+}
